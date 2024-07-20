cb23947104e575cd31ef9d62a6439d161723c69d
ijwgt <66661966@bk.ru>
2024-07-20 17:42:52 +0300

    initial

---
+++ src/js/map.js
@@ -0,0 +1,16 @@
+export default class ErrorRepository {
+	constructor() {
+		this.codes = new Map();
+	}
+
+	addError(code, description) {
+		this.codes.set(code, description);
+	}
+
+	translate(code) {
+		if(!this.codes.has(code)){
+		  return 'Unknown error';
+		}
+	   return this.codes.get(code);
+	  }
+}
